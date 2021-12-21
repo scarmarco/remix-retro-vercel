cf8808188cf22697a909ae0e88b8074ce30580d0
Marcelo Risoli <celorisoli@gmail.com>
2021-12-21 08:37:30 -0300

    Revert "add request to server for board data"
    This reverts commit c3b8bc4f778bd2680477658a651b3f251ac6a81b.

--- app/routes/board/$boardId.tsx
+++ app/routes/board/$boardId.tsx
@@ -1,32 +1,6 @@
-import { useLoaderData, useParams } from "remix";
-import type { LoaderFunction } from "remix";
-import invariant from "tiny-invariant";
-import { gql, GraphQLClient } from 'graphql-request'
+import { useParams } from "remix";
 
 import Card from "~/components/Card";
-const endpoint = 'https://3k5q3ochfnaxngc2oxr37ioemu.appsync-api.eu-west-1.amazonaws.com/graphql'
-
-const query = gql`
-  query getBoard($boardId: ID!) {
-    getBoardById(boardId: $boardId) {
-      id
-      stage
-      team
-    }
-  }
-`
-const graphQLClient = new GraphQLClient(endpoint, {
-  headers: {
-    'x-api-key': process.env.API_KEY || '',
-  },
-})
-
-export let loader: LoaderFunction = async ({ params }) => {
-  invariant(params.boardId, "Expected params.boardId");
-  const data = await graphQLClient.request(query, params)
-  console.log(JSON.stringify(data, undefined, 2))
-  return null
-}
 
 const items = [
   { id: 1, text: "some" },
@@ -35,11 +9,11 @@ const items = [
 ];
 
 export default function BoardRoute() {
-  const board = useLoaderData();
   const params = useParams();
+  console.log(params.boardId);
 
   return (
-    <div className="flex h-full p-3 bg-gray-300 gap-3">
+    <div className="h-full bg-gray-300 flex p-3 gap-3">
       <div className="flex-1">
         <Card placeholder="It worked" items={items} />
       </div>
