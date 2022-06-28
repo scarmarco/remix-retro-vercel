07b50a269248cfe895afe736ed8cfadd766d08eb
Marco Scarciofolo <marco.scarciofolo@typeform.com>
2022-06-28 11:29:48 +0200

    fix board stream

--- app/routes/board/$boardId.events.ts
+++ app/routes/board/$boardId.events.ts
@@ -1,68 +1,81 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { Comment } from "@prisma/client";
 import invariant from "tiny-invariant";
-import { EventEmitter } from "node:events";
-import { events, initializeBoardEmitter } from "~/services/board.server";
+import { events } from "~/services/board.server";
 import { eventType } from "~/utils";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   invariant(params.boardId, "Expected params.boardId");
   if (!request.signal) return new Response(null, { status: 500 });
 
-  const { boardId } = params;
-
   return new Response(
     new ReadableStream({
       start(controller) {
         const encoder = new TextEncoder();
 
-        initializeBoardEmitter(boardId);
-
-        const handleNewComment = (comment: Comment) => {
-          controller.enqueue(
-            encoder.encode(`event: ${eventType.NEW_COMMENT}\n`)
-          );
-          controller.enqueue(
-            encoder.encode(`data: ${JSON.stringify(comment)}\n\n`)
-          );
+        const handleNewComment = (boardId: string, comment: Comment) => {
+          if (boardId === params.boardId) {
+            controller.enqueue(
+              encoder.encode(`event: ${eventType.NEW_COMMENT}\n`)
+            );
+            controller.enqueue(
+              encoder.encode(`data: ${JSON.stringify(comment)}\n\n`)
+            );
+          }
         };
 
-        const handleGroupComment = (comment: Comment, childrenId: string) => {
-          controller.enqueue(
-            encoder.encode(`event: ${eventType.GROUP_COMMENT}\n`)
-          );
-          controller.enqueue(
-            encoder.encode(
-              `data: ${JSON.stringify({ comment, childrenId })}\n\n`
-            )
-          );
+        const handleGroupComment = (
+          boardId: string,
+          comment: Comment,
+          childrenId: string
+        ) => {
+          if (boardId === params.boardId) {
+            controller.enqueue(
+              encoder.encode(`event: ${eventType.GROUP_COMMENT}\n`)
+            );
+            controller.enqueue(
+              encoder.encode(
+                `data: ${JSON.stringify({ comment, childrenId })}\n\n`
+              )
+            );
+          }
         };
 
-        const handleUngroupComment = (comment: Comment, childrenId: string) => {
-          controller.enqueue(
-            encoder.encode(`event: ${eventType.UNGROUP_COMMENT}\n`)
-          );
-          controller.enqueue(
-            encoder.encode(
-              `data: ${JSON.stringify({ comment, childrenId })}\n\n`
-            )
-          );
+        const handleUngroupComment = (
+          boardId: string,
+          comment: Comment,
+          childrenId: string
+        ) => {
+          if (boardId === params.boardId) {
+            controller.enqueue(
+              encoder.encode(`event: ${eventType.UNGROUP_COMMENT}\n`)
+            );
+            controller.enqueue(
+              encoder.encode(
+                `data: ${JSON.stringify({ comment, childrenId })}\n\n`
+              )
+            );
+          }
         };
 
-        const handleLikeComment = (comment: Comment) => {
-          controller.enqueue(
-            encoder.encode(`event: ${eventType.LIKE_COMMENT}\n`)
-          );
-          controller.enqueue(
-            encoder.encode(`data: ${JSON.stringify(comment)}\n\n`)
-          );
+        const handleLikeComment = (boardId: string, comment: Comment) => {
+          if (boardId === params.boardId) {
+            controller.enqueue(
+              encoder.encode(`event: ${eventType.LIKE_COMMENT}\n`)
+            );
+            controller.enqueue(
+              encoder.encode(`data: ${JSON.stringify(comment)}\n\n`)
+            );
+          }
         };
 
-        const handleChangeStage = (stage: string) => {
-          controller.enqueue(
-            encoder.encode(`event: ${eventType.CHANGE_STAGE}\n`)
-          );
-          controller.enqueue(encoder.encode(`data: ${stage}\n\n`));
+        const handleChangeStage = (boardId: string, stage: string) => {
+          if (boardId === params.boardId) {
+            controller.enqueue(
+              encoder.encode(`event: ${eventType.CHANGE_STAGE}\n`)
+            );
+            controller.enqueue(encoder.encode(`data: ${stage}\n\n`));
+          }
         };
 
         let closed = false;
@@ -70,46 +83,24 @@ export const loader: LoaderFunction = async ({ request, params }) => {
           if (closed) return;
           closed = true;
 
-          events[boardId].removeListener(
-            eventType.NEW_COMMENT,
-            handleNewComment
-          );
-          events[boardId].removeListener(
-            eventType.GROUP_COMMENT,
-            handleGroupComment
-          );
-          events[boardId].removeListener(
+          events.removeListener(eventType.NEW_COMMENT, handleNewComment);
+          events.removeListener(eventType.GROUP_COMMENT, handleGroupComment);
+          events.removeListener(
             eventType.UNGROUP_COMMENT,
             handleUngroupComment
           );
-          events[boardId].removeListener(
-            eventType.LIKE_COMMENT,
-            handleLikeComment
-          );
-          events[boardId].removeListener(
-            eventType.CHANGE_STAGE,
-            handleChangeStage
-          );
-
-          if (events[boardId].listenerCount(eventType.NEW_COMMENT) === 0) {
-            delete events[boardId];
-          }
+          events.removeListener(eventType.LIKE_COMMENT, handleLikeComment);
+          events.removeListener(eventType.CHANGE_STAGE, handleChangeStage);
 
           request.signal.removeEventListener("abort", close);
           controller.close();
         };
 
-        events[boardId].addListener(eventType.NEW_COMMENT, handleNewComment);
-        events[boardId].addListener(
-          eventType.GROUP_COMMENT,
-          handleGroupComment
-        );
-        events[boardId].addListener(
-          eventType.UNGROUP_COMMENT,
-          handleUngroupComment
-        );
-        events[boardId].addListener(eventType.LIKE_COMMENT, handleLikeComment);
-        events[boardId].addListener(eventType.CHANGE_STAGE, handleChangeStage);
+        events.addListener(eventType.NEW_COMMENT, handleNewComment);
+        events.addListener(eventType.GROUP_COMMENT, handleGroupComment);
+        events.addListener(eventType.UNGROUP_COMMENT, handleUngroupComment);
+        events.addListener(eventType.LIKE_COMMENT, handleLikeComment);
+        events.addListener(eventType.CHANGE_STAGE, handleChangeStage);
         request.signal.addEventListener("abort", close);
 
         if (request.signal.aborted) {
--- app/services/board.server.ts
+++ app/services/board.server.ts
@@ -3,24 +3,15 @@ import { Comment } from "@prisma/client";
 import { eventType } from "~/utils";
 
 declare global {
-  var boardEvents: Record<string, EventEmitter>;
+  var boardEvents: EventEmitter;
 }
 
-global.boardEvents = global.boardEvents || {};
+global.boardEvents = global.boardEvents || new EventEmitter();
 
 export const events = global.boardEvents;
 
-export function initializeBoardEmitter(boardId: string) {
-  if (!global.boardEvents[boardId]) {
-    console.log("new");
-    console.log(global.boardEvents);
-    global.boardEvents[boardId] = new EventEmitter();
-  }
-}
-
 export function newComment(boardId: string, comment: Comment) {
-  console.log(global.boardEvents[boardId]);
-  boardEvents[boardId].emit(eventType.NEW_COMMENT, comment);
+  boardEvents.emit(eventType.NEW_COMMENT, boardId, comment);
 }
 
 export function groupComment(
@@ -28,7 +19,7 @@ export function groupComment(
   comment: Comment,
   childrenId: string
 ) {
-  boardEvents[boardId].emit(eventType.GROUP_COMMENT, comment, childrenId);
+  boardEvents.emit(eventType.GROUP_COMMENT, boardId, comment, childrenId);
 }
 
 export function ungroupComment(
@@ -36,13 +27,13 @@ export function ungroupComment(
   comment: Comment,
   childrenId: string
 ) {
-  boardEvents[boardId].emit(eventType.UNGROUP_COMMENT, comment, childrenId);
+  boardEvents.emit(eventType.UNGROUP_COMMENT, boardId, comment, childrenId);
 }
 
 export function likeComment(boardId: string, comment: Comment) {
-  boardEvents[boardId].emit(eventType.LIKE_COMMENT, comment);
+  boardEvents.emit(eventType.LIKE_COMMENT, boardId, comment);
 }
 
 export function changeStage(boardId: string, stage: string) {
-  boardEvents[boardId].emit(eventType.CHANGE_STAGE, stage);
+  boardEvents.emit(eventType.CHANGE_STAGE, boardId, stage);
 }
